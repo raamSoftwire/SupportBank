8e64837cd90456c2e9c1d3b302724d46a544e066
Raam Chauhan <Raam.Chauhan@softwire.com>
2017-11-08 13:47:19 +0000

    Formatted properties in transactionClass

--- transactionClass.js
+++ transactionClass.js
@@ -1,10 +1,22 @@
+var moment = require('moment');
+
+
+var formatter = new Intl.NumberFormat('en-UK', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 2,
+    // the default value for minimumFractionDigits depends on the currency
+    // and is usually already 2
+});
+
+
 class Transaction {
     constructor(date, from, to, narrative, amount) {
-        this.date = date;
+        this.date = moment(date,"DD/MM/YYYY",true).format("DD/MM/YYYY");
         this.from = from;
         this.to = to;
         this.narrative = narrative;
-        this.amount = amount;
+        this.amount = formatter.format(amount);
     }
 }
 
