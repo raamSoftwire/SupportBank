9d8c7602e5e67193671355cc2db890fa6592a0c0
Raam Chauhan <Raam.Chauhan@softwire.com>
2017-11-08 10:50:47 +0000

    Read in Transactions2014.csv

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+
+var fs = require('fs'); //file system
+var array = fs.readFileSync('Transactions2014.csv').toString().split("\n");
+
+
+// for(i in array) {
+//     console.log(array[i]);
+// }
+
+console.log(array[1].split(","))
