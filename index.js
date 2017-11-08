e8286f594a54f83bf45444c18bb95b816fff292c
Raam Chauhan <Raam.Chauhan@softwire.com>
2017-11-08 12:22:52 +0000

    Created transactionClass

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ var inputArray = fs.readFileSync('Transactions2014.csv').toString();
 var parse = require('csv-parse/lib/sync');
 records = parse(inputArray, {columns: true});
 
+const Transaction = require('C:\\Work\\Training\\SupportBank\\transactionClass.js');
+
+transactions = [];
+
 for(i in records)
 {
-    console.log(records[i]);
+    transactions[i] = new Transaction(
+        records[i]['Date'],
+        records[i]['From'],
+        records[i]['To'],
+        records[i]['Narrative'],
+        records[i]['Amount'])
 }
 
-
-
+console.log(transactions[0])
---
+++ transactionClass.js
@@ -0,0 +1,11 @@
+class Transaction {
+    constructor(date, from, to, narrative, amount) {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.narrative = narrative;
+        this.amount = amount;
+    }
+}
+
+module.exports = Transaction;
