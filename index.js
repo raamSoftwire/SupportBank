1484bc6e673426b714eee8db1b8cee0170ac87cf
Raam Chauhan <Raam.Chauhan@softwire.com>
2017-11-09 15:51:17 +0000

    Added parseTransactions() module

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const Transaction = require('./transactionClass');
 
 const accountExistsQ = require('./accountExistsQ');
 const createAccounts = require('./createAccounts');
+const parseTransactions = require('./parseTransactions');
 
 const formatter = new Intl.NumberFormat('en-UK', {
     style: 'currency',
@@ -20,40 +21,7 @@ log4js.configure({
     categories: {default: { appenders: ['file'], level: 'debug'}}
 });
 
-function parseTransactions(){
-    var keys = Object.keys(records[1]);
 
-    for(let i in records)
-    {
-        let lineValue = parseInt(i) + 2; // +1 for header row, +1 for zero base
-
-        if(!moment(records[i]['Date'],["DD/MM/YYYY","YYYY-MM-DD"],'en').isValid())
-        {
-            logger.error("Invalid date found in line " + lineValue +
-                " : " + records[i]['Date']);
-        }
-
-        else if (isNaN(parseFloat(records[i]['Amount'])))
-        {
-            logger.error("Invalid amount found in line " + lineValue
-                + " : " + records[i]['Amount'])
-        }
-        else
-        {
-            transaction = new Transaction(
-                records[i][keys[0]],
-                records[i][keys[1]],
-                records[i][keys[2]],
-                records[i][keys[3]],
-                records[i][keys[4]]);
-
-            transactions.push(transaction);
-            //transaction not added to list of transactions
-        }
-    }
-    logger.info("Successfully parsed " + transactions.length + " of " + records.length +
-        " transactions");
-}
 
 
 
---
+++ parseTransactions.js
@@ -0,0 +1,42 @@
+const log4js = require('log4js');
+const moment = require('moment');
+const Transaction = require('./transactionClass');
+
+const logger = log4js.getLogger('parseTransactions.js');
+
+function parseTransactions(){
+    var keys = Object.keys(records[1]);
+
+    for(let i in records)
+    {
+        let lineValue = parseInt(i) + 2; // +1 for header row, +1 for zero base
+
+        if(!moment(records[i]['Date'],["DD/MM/YYYY","YYYY-MM-DD"],'en').isValid())
+        {
+            logger.error("Invalid date found in line " + lineValue +
+                " : " + records[i]['Date']);
+        }
+
+        else if (isNaN(parseFloat(records[i]['Amount'])))
+        {
+            logger.error("Invalid amount found in line " + lineValue
+                + " : " + records[i]['Amount'])
+        }
+        else
+        {
+            transaction = new Transaction(
+                records[i][keys[0]],
+                records[i][keys[1]],
+                records[i][keys[2]],
+                records[i][keys[3]],
+                records[i][keys[4]]);
+
+            transactions.push(transaction);
+            //transaction not added to list of transactions
+        }
+    }
+    logger.info("Successfully parsed " + transactions.length + " of " + records.length +
+        " transactions");
+}
+
+module.exports = parseTransactions;
